b11c8e0fe5e6efacc8399f22de555a2d5d821843
Daley Mottley <daley.mottley@hotmail.com>
2024-08-06 10:28:55 +0000

    Bind PORT variable

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ def generate_report_id():
     return str(int(datetime.datetime.now().timestamp()))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
