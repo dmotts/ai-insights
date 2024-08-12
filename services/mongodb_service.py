b87cb1cbde5fed95bdeccb6dd4c0b49fc4a9daeb
Daley Mottley <daley.mottley@hotmail.com>
2024-08-12 17:07:22 +0000

    Refactor configuration of mongodb

--- services/mongodb_service.py
+++ services/mongodb_service.py
@@ -1,32 +1,45 @@
 import logging
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from config import Config
 
 class MongoDBService:
     def __init__(self):
         """
-        Initializes the MongoDBService with MongoDB client.
+        Initializes the MongoDBService with MongoDB client and verifies connection.
         """
         self.logger = logging.getLogger(__name__)
 
-        # Initialize MongoDB client
-        self.client = MongoClient(Config.MONGODB_URI)
-        self.db = self.client[Config.MONGODB_DATABASE_NAME]
-        self.collection_name = Config.MONGODB_COLLECTION_NAME
-        self.collection = self.db[self.collection_name]
+        try:
+            # Initialize MongoDB client with Server API version 1
+            self.client = MongoClient(Config.MONGODB_URI, server_api=ServerApi('1'))
 
-        # Ensure collection is created
-        self._ensure_collection()
+            # Ping the server to confirm a successful connection
+            self.client.admin.command('ping')
+            self.logger.info("Pinged your deployment. Successfully connected to MongoDB!")
+
+            self.db = self.client[Config.MONGODB_DATABASE_NAME]
+            self.collection_name = Config.MONGODB_COLLECTION_NAME
+            self.collection = self.db[self.collection_name]
+
+            # Ensure collection is created
+            self._ensure_collection()
+        except Exception as e:
+            self.logger.error(f"Failed to connect to MongoDB: {e}")
+            raise
 
     def _ensure_collection(self):
         """
         Ensures the collection exists in the database.
         """
-        if self.collection_name not in self.db.list_collection_names():
-            self.db.create_collection(self.collection_name)
-            self.logger.info(f'Collection {self.collection_name} created successfully')
-        else:
-            self.logger.info(f'Collection {self.collection_name} already exists')
+        try:
+            if self.collection_name not in self.db.list_collection_names():
+                self.db.create_collection(self.collection_name)
+                self.logger.info(f'Collection {self.collection_name} created successfully')
+            else:
+                self.logger.info(f'Collection {self.collection_name} already exists')
+        except Exception as e:
+            self.logger.error(f'Error ensuring collection exists: {e}')
 
     def save_report_data(self, report_data: dict):
         """
