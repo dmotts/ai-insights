cd442579a55583c9c7f16f5ea7d67e647c6a3e14
Daley Mottley <daley.mottley@hotmail.com>
2024-08-14 20:01:29 +0000

    Refactor config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
                 "private_key_id": os.getenv('GOOGLE_SHEETS_PRIVATE_KEY_ID', ''),
                 "private_key": os.getenv('GOOGLE_SHEETS_PRIVATE_KEY', '').replace('\\n', '\n'),
                 "client_email": os.getenv('GOOGLE_SHEETS_CLIENT_EMAIL', ''),
-                "client_id": os.getenv('GOOGLE_SHEETS_CLIENT_ID', ''),
+            "client_id": os.getenv('GOOGLE_SHEETS_CLIENT_ID', ''),
                 "auth_uri": os.getenv('GOOGLE_SHEETS_AUTH_URI', 'https://accounts.google.com/o/oauth2/auth'),
                 "token_uri": os.getenv('GOOGLE_SHEETS_TOKEN_URI', 'https://oauth2.googleapis.com/token'),
                 "auth_provider_x509_cert_url": os.getenv('GOOGLE_SHEETS_AUTH_PROVIDER_X509_CERT_URL', 'https://www.googleapis.com/oauth2/v1/certs'),
